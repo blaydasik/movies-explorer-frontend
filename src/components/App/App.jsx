20d65725717db4cf51ab66600c64441851dd5b38
Алексей Куров <alexei.s.kurov@gmail.com>
2023-01-16 03:06:04 +0300

    feat: Header in development

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,26 @@
+import { useEffect, useState } from "react";
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
+
 // импортируем все нужные компоненты
 import './App.css';
 import Main from '../Main/Main';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 
+import { CurrentUserContext } from '../../contexts/CurrentUserContext';
+
 function App() {
+
+//переменные состояния
+const [currentUser, setCurrentUser] = useState({});
+const [loggedIn, setLoggedIn] = useState(false);
+
   return (
-    <div className="App">
+    <CurrentUserContext.Provider value={{ currentUser, loggedIn }}>
       <Header></Header>
       <Main></Main>
       <Footer />
-    </div>
+    </CurrentUserContext.Provider>
   );
 }
 
